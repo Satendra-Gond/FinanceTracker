c9fec1d02b736dbafe06516aaa55adfb1faf2a54
satendra3406 <satendragond@startbitsolutions.com>
2021-04-02 17:24:55 +0530

    Add stock lookup form and regular stock lookup functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  get 'search_stock', to: 'stocks#search'
   get 'my_portfolio', to: 'users#my_portfolio'
   devise_for :users
   root 'welcome#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
